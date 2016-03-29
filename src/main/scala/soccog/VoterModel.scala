d1652ac8ee9043c25fd4fdc6a515f182eb01ef55
Ricardo Honorato-Zimmer <rikardo.horo@gmail.com>
2016-03-29 22:03:13 +0200

    refactored inner functions out of runOnceOpt

--- src/main/scala/soccog/VoterModel.scala
+++ src/main/scala/soccog/VoterModel.scala
@@ -228,28 +228,43 @@ object VoterModel {
     }
   }
 
-  def runOnceOpt(fileName: String): Unit = {
-    val m = new VoterModel(social, cognitive, temperature)
-    // create random initial graph
-    val rnd = new Random()
-    // about 10 friends per node
-    m.randomised(meanDegree / m.numNodes, rnd)
-    // observables
-    var freshId: Int = 0
-    val idOfCogstate = mutable.Map.empty[Seq[Int], Int]
-    val cogstateOfId = mutable.Map.empty[Int, Seq[Int]]
-    val cogstateIdOfNode = mutable.Map.empty[m.Node, Int]
-    val cogstateCount = mutable.Map.empty[Int, Int]
-    val optimals = mutable.Set.empty[Int]
+  def maxBy(xs: Iterable[Int], f: Int => Double): (Int, Double) = {
+    var max: Int = -1
+    var maxValue: Double = 0.0
+    for (x <- xs) {
+      val fx = f(x)
+      if (fx > maxValue) {
+        max = x
+        maxValue = fx
+      }
+    }
+    (max, maxValue)
+  }
+
+  // cog-states observables
+  var freshId: Int = 0
+  val idOfCogstate = mutable.Map.empty[Seq[Int], Int]
+  val cogstateOfId = mutable.Map.empty[Int, Seq[Int]]
+  val cogstateIdOfNode = mutable.Map.empty[VoterModel#Node, Int]
+  val cogstateCount = mutable.Map.empty[Int, Int]
+  val optimals = mutable.Set.empty[Int]
+
+  def initCogstates(m: VoterModel): Unit = {
+    idOfCogstate.clear
+    cogstateOfId.clear
+    cogstateIdOfNode.clear
+    cogstateCount.clear
+    optimals.clear
+    freshId = 0
     for (x <- m.nodes) {
       val v =
         (for (i <- 0 until (m.numConcepts-1);
               j <- (i+1) until m.numConcepts)
          yield x.beliefs(i)(j)).toSeq
-      val id = if (idOfCogstate contains v) {
+      if (idOfCogstate contains v) {
         val id = idOfCogstate(v)
         cogstateCount(id) += 1
-        id
+        cogstateIdOfNode(x) = id
       } else {
         val id = freshId
         freshId += 1
@@ -257,64 +272,79 @@ object VoterModel {
         cogstateOfId(id) = v
         cogstateCount(id) = 1
         if (x.isOptimal) optimals += id
-        id
+        cogstateIdOfNode(x) = id
       }
-      cogstateIdOfNode(x) = id
     }
+  }
+
+  def opts: Int = optimals.toSeq.map(cogstateCount(_)).sum
+  def distincts: Int = optimals.count(cogstateCount(_) > 0)
+
+  // i and j are cogstate ids
+  def shared(m: VoterModel, i: Int, j: Int): Double = {
+    val n = m.numConcepts * (m.numConcepts-1) / 2
+    val u = cogstateOfId(i)
+    val v = cogstateOfId(j)
+    require(u.size == n, s"${u.size} != $n")
+    require(v.size == n, s"${v.size} != $n")
+    var sames = 0
+    for (i <- 0 until n if u(i) == v(i)) sames += 1
+    require(sames <= n, s"$sames > n")
+    sames.toDouble / n
+  }
+
+  def mostPopular(p: (Int => Boolean) = (_ => true))
+      : mutable.Set[Int] = {
+    var max: Int = 0
+    val ids = mutable.Set.empty[Int]
+    for ((i, n) <- cogstateCount if p(i)) {
+      if (n > max) {
+        max = n
+        ids.clear
+        ids += i
+      } else if (n == max) {
+        ids += i
+      }
+    }
+    ids
+  }
+
+  def homogeneity(m: VoterModel, i: Int, p: (Int => Boolean) = (_ => true)): Double =
+    (for ((j, n) <- cogstateCount if (n > 0) && p(j))
+     yield n * shared(m, i, j)).sum
+
+  def dissent(m: VoterModel, i: Int, numDissidents: Int,
+    p: (Int => Boolean) = (_ => true)): Double =
+    (for ((j, n) <- cogstateCount if (n > 0) && (j != i) && p(j)) yield
+      (n.toDouble / numDissidents) * (1.0 - shared(m, i, j))).sum
+
+  def runOnceOpt(fileName: String): Unit = {
+    val m = new VoterModel(social, cognitive, temperature, numNodes, numConcepts)
+    // create random initial graph
+    val rnd = new Random()
+    // about 10 friends per node
+    m.randomised(meanDegree / m.numNodes, rnd)
+    // observables
+    initCogstates(m)
+    // memoised version of shared
     val sharedmap = mutable.Map.empty[(Int, Int), Double]
-    def opts: Int = optimals.toSeq.map(cogstateCount(_)).sum
-    def distincts: Int = optimals.count(cogstateCount(_) > 0)
-    def shared(i: Int, j: Int): Double =
+    def mshared(i: Int, j: Int): Double =
       sharedmap get (i, j) match {
         case Some(s) => s
         case None => {
-          val n = m.numConcepts * (m.numConcepts-1) / 2
-          val u = cogstateOfId(i)
-          val v = cogstateOfId(j)
-          require(u.size == n, s"${u.size} != $n")
-          require(v.size == n, s"${v.size} != $n")
-          var sames = 0
-          for (i <- 0 until n if u(i) == v(i)) sames += 1
-          require(sames <= n, s"$sames > n")
-          val fraction = sames.toDouble / n
+          val fraction = shared(m, i, j)
           sharedmap((i, j)) = fraction
           sharedmap((j, i)) = fraction
           fraction
         }
       }
-    def mostPopular(p: (Int => Boolean) = (_ => true))
-        : mutable.Set[Int] = {
-      var max: Int = 0
-      val ids = mutable.Set.empty[Int]
-      for ((i, n) <- cogstateCount if p(i)) {
-        if (n > max) {
-          max = n
-          ids.clear
-          ids += i
-        } else if (n == max) {
-          ids += i
-        }
-      }
-      ids
-    }
-    def maxBy(xs: Iterable[Int], f: Int => Double): (Int, Double) = {
-      var max: Int = -1
-      var maxValue: Double = 0.0
-      for (x <- xs) {
-        val fx = f(x)
-        if (fx > maxValue) {
-          max = x
-          maxValue = fx
-        }
-      }
-      (max, maxValue)
-    }
     def homogeneity(i: Int, p: (Int => Boolean) = (_ => true)): Double =
       (for ((j, n) <- cogstateCount if (n > 0) && p(j))
-       yield n * shared(i, j)).sum
-    def dissent(i: Int, numDissidents: Int, p: (Int => Boolean) = (_ => true)): Double =
+       yield n * mshared(i, j)).sum
+    def dissent(i: Int, numDissidents: Int,
+      p: (Int => Boolean) = (_ => true)): Double =
       (for ((j, n) <- cogstateCount if (n > 0) && (j != i) && p(j)) yield
-        (n.toDouble / numDissidents) * (1.0 - shared(i, j))).sum
+        (n.toDouble / numDissidents) * (1.0 - mshared(i, j))).sum
     // track social and cognitive energy
     var se = m.socialEnergy
     var ce = m.cognitiveEnergy
@@ -326,10 +356,6 @@ object VoterModel {
       out.print("\"total dissidents\" \"optimal dissidents\" total soc cog ")
       out.println("\"successful moves\"")
     }
-    // var lastHg: Double = 0.0
-    // var lastOptHg: Double = 0.0
-    // var lastDis: Double = 0.0
-    // var lastOptDis: Double = 0.0
     var lastId: Int = -1
     var lastOptId: Int = -1
     var changed: Int = 0
@@ -337,12 +363,12 @@ object VoterModel {
     for (sc <- 0 until numSteps) {
       val popIds = mostPopular()
       val optPopIds = mostPopular(optimals)
-      val (maxId, maxHg) = maxBy(popIds, i => homogeneity(i))
-        // if (popIds(lastId)) (lastId, homogeneity(lastId))
-        // else maxBy(popIds, i => homogeneity(i))
-      val (maxOptId, maxOptHg) = maxBy(optPopIds, i => homogeneity(i, optimals))
-        // if (optPopIds(lastOptId)) (lastOptId, homogeneity(lastOptId))
-        // else maxBy(optPopIds, i => homogeneity(i, optimals))
+      val (maxId, maxHg) = // maxBy(popIds, i => homogeneity(i))
+        if (popIds(lastId)) (lastId, homogeneity(lastId))
+        else maxBy(popIds, i => homogeneity(i))
+      val (maxOptId, maxOptHg) = // maxBy(optPopIds, i => homogeneity(i, optimals))
+        if (optPopIds(lastOptId)) (lastOptId, homogeneity(lastOptId))
+        else maxBy(optPopIds, i => homogeneity(i, optimals))
       val numDissidents = (for ((j, n) <- cogstateCount
         if (n > 0) && (j != maxId)) yield n).sum
       val dis = dissent(maxId, numDissidents)
@@ -350,23 +376,8 @@ object VoterModel {
         if (n > 0) && (j != maxOptId) && optimals(j)) yield n).sum
       val optDis = dissent(maxOptId, numOptDissidents, optimals)
       // TODO: can the creation of a new cogstate increase the
-      // total homogeneity by more than 1?  Yes it can!
-      // ** what's the maximum possible change then? **
-      // if (sc > 0) {
-      //   val tolerance = 1
-      //   require((maxHg < lastHg + tolerance) && (maxHg > lastHg - tolerance),
-      //     s"maxHg=$maxHg, lastHg=$lastHg, maxId=$maxId, lastId=$lastId, cogstateCount(maxId)=${cogstateCount(maxId)}")
-      //   require((maxOptHg < lastOptHg + tolerance) && (maxOptHg > lastOptHg - tolerance),
-      //     s"maxOptHg=$maxOptHg, lastOptHg=$lastOptHg, maxOptId=$maxOptId, lastOptId=$lastOptId")
-      //   require((dis < lastDis + tolerance) && (dis > lastDis - tolerance),
-      //     s"dis=$dis, lastDis=$lastDis, maxId=$maxId, lastId=$lastId")
-      //   require((optDis < lastOptDis + tolerance) && (optDis > lastOptDis - tolerance),
-      //     s"optDis=$optDis, lastOptDis=$lastOptDis, maxOptId=$maxOptId, lastOptId=$lastOptId")
-      // }
-      // lastHg = maxHg
-      // lastOptHg = maxOptHg
-      // lastDis = dis
-      // lastOptDis = optDis
+      // total homogeneity by more than 1?  Yes it can! How?
+      // What's the maximum possible change?
       lastId = maxId
       lastOptId = maxOptId
       writeToFile (fileName, true) { out =>
@@ -379,7 +390,6 @@ object VoterModel {
         val oldcogstate = cogstateIdOfNode(x)
         cogstateCount(oldcogstate) -= 1
         if (cogstateCount(oldcogstate) == 0) {
-          // println(s"sharedmap.size = ${sharedmap.size}")
           val keys = for ((i, j) <- sharedmap.keys
             if (i == oldcogstate) || (j == oldcogstate)) yield (i, j)
           for (k <- keys) sharedmap -= k
@@ -516,7 +526,8 @@ object VoterModel {
       val nn = m.nodes.zipWithIndex.toMap
       for (x <- m.nodes) {
         val cog = x.cognitiveEnergy/m.cognitive
-        val f = 1.0-((cog+120)/240)
+        val t = m.numConcepts*(m.numConcepts-1)*(m.numConcepts-2)/6
+        val f = 1.0-((cog+t)/(t*2))
         val alpha = (f*f*255).toInt
         require(alpha < 256, s"$alpha > 256")
         var numNegativeBeliefs = 0
